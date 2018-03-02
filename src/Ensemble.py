fc07a2f2f071b36440df989e5b02c67811001065
Mike Shann <mikeshann@gmx.net>
2018-03-02 21:34:29 +0100

    ensemble linear regression and SVR rmsle = 0.11524 on public leaderboard

---
+++ src/Ensemble.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from scipy.stats import pearsonr
+
+predictionsLR = pd.read_csv('../data/submission.csv')
+predictionsSVR = pd.read_csv('../data/submissionSVR.csv')
+print predictionsLR["Id"].values
+predictionsEnsemble = (predictionsLR['SalePrice'].values + predictionsSVR['SalePrice'].values)/2
+solution = pd.DataFrame({"Id":predictionsLR["Id"].values, "SalePrice":predictionsEnsemble})
+solution.set_index("Id")
+solution.to_csv("../data/submissionEnsemble.csv", index = False)
+
+print pearsonr(predictionsLR['SalePrice'].values, predictionsSVR['SalePrice'].values)
